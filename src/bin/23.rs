80ad88a07651de9428269886562ea0adfa153cee
Anurag Bakshi <anuragbakshi123@gmail.com>
2022-12-27 20:23:52 -0500

    finish 23

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -1,9 +1,222 @@
+#![feature(variant_count)]
+#![feature(generators)]
+#![feature(iter_from_generator)]
+
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+};
+
+use enum_utils::IterVariants;
+use itertools::Itertools;
+use maplit::hashmap;
+
+#[derive(Clone, IterVariants)]
+enum Dir {
+    North,
+    South,
+    West,
+    East,
+}
+
+use Dir::*;
+
+impl Dir {
+    fn delta(&self) -> (isize, isize) {
+        match self {
+            North => (-1, 0),
+            South => (1, 0),
+            West => (0, -1),
+            East => (0, 1),
+        }
+    }
+
+    fn orthogonal(&self) -> [Dir; 2] {
+        match self {
+            North | South => [East, West],
+            West | East => [North, South],
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Pos {
+    x: isize,
+    y: isize,
+}
+
+impl Pos {
+    fn adj(&self, dir: &Dir) -> Self {
+        let (dx, dy) = dir.delta();
+
+        Self {
+            x: self.x + dx,
+            y: self.y + dy,
+        }
+    }
+
+    fn neighbors(&self) -> impl Iterator<Item = Pos> + '_ {
+        std::iter::from_generator(|| {
+            for dx in -1..=1 {
+                for dy in -1..=1 {
+                    if !(dx == 0 && dy == 0) {
+                        yield Pos {
+                            x: self.x + dx,
+                            y: self.y + dy,
+                        }
+                    }
+                }
+            }
+        })
+    }
+}
+
+struct Elf {
+    pos: Pos,
+}
+
+struct State {
+    elfs: HashMap<Pos, Elf>,
+    dirs: Vec<Dir>,
+}
+impl State {
+    fn step(&mut self) -> bool {
+        let mut proposals_from = hashmap! {};
+
+        // step 1
+        for (_, e) in self.elfs.iter() {
+            let any_neighbors = e.pos.neighbors().any(|p| self.elfs.contains_key(&p));
+
+            if any_neighbors {
+                for dir_to_try in self.dirs.iter() {
+                    let can_move = !self.elfs.contains_key(&e.pos.adj(dir_to_try))
+                        && dir_to_try
+                            .orthogonal()
+                            .iter()
+                            .all(|d| !self.elfs.contains_key(&e.pos.adj(dir_to_try).adj(d)));
+
+                    if can_move {
+                        proposals_from
+                            .entry(e.pos.adj(dir_to_try))
+                            .or_insert(vec![])
+                            .push(e.pos.clone());
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        let mut any_moved = false;
+
+        // step 2
+        for (pos, from) in proposals_from.iter() {
+            if from.len() == 1 {
+                let from = &from[0];
+                let mut elf = self.elfs.remove(from).unwrap();
+
+                elf.pos = pos.clone();
+                self.elfs.insert(pos.clone(), elf);
+
+                any_moved = true;
+            }
+        }
+
+        let first_dir = self.dirs.remove(0);
+        self.dirs.push(first_dir);
+
+        any_moved
+    }
+
+    fn elf_bbox(&self) -> (Pos, Pos) {
+        let mut min_x = 0;
+        let mut min_y = 0;
+        let mut max_x = 0;
+        let mut max_y = 0;
+
+        for (p, _) in self.elfs.iter() {
+            min_x = min(min_x, p.x);
+            min_y = min(min_y, p.y);
+            max_x = max(max_x, p.x);
+            max_y = max(max_y, p.y);
+        }
+
+        (Pos { x: min_x, y: min_y }, Pos { x: max_x, y: max_y })
+    }
+
+    fn captured_area(&self) -> isize {
+        let (min, max) = self.elf_bbox();
+
+        (max.x - min.x + 1) * (max.y - min.y + 1)
+    }
+
+    fn print(&self) {
+        let (min, max) = self.elf_bbox();
+
+        for x in min.x..=max.x {
+            for y in min.y..=max.y {
+                if self.elfs.contains_key(&Pos { x, y }) {
+                    print!("#");
+                } else {
+                    print!(".")
+                }
+            }
+
+            println!();
+        }
+    }
+}
+
+fn parse_input(input: &str) -> State {
+    let mut elfs = hashmap! {};
+
+    let as_vec = input
+        .lines()
+        .enumerate()
+        .map(|(x, l)| {
+            l.chars()
+                .enumerate()
+                .filter_map(|(y, c)| match c {
+                    '#' => Some(Elf {
+                        pos: Pos {
+                            x: x as isize,
+                            y: y as isize,
+                        },
+                    }),
+                    _ => None,
+                })
+                .collect_vec()
+        })
+        .concat();
+
+    for e in as_vec {
+        elfs.insert(e.pos.clone(), e);
+    }
+
+    State {
+        elfs,
+        dirs: Dir::iter().collect(),
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut state = parse_input(input);
+    for _ in 0..10 {
+        state.step();
+    }
+
+    Some((state.captured_area() - state.elfs.len() as isize) as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut state = parse_input(input);
+
+    let mut steps = 1;
+    while state.step() {
+        steps += 1;
+    }
+
+    Some(steps)
 }
 
 fn main() {
@@ -19,12 +232,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 23);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(110));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 23);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(20));
     }
 }
--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,9 +1,234 @@
+use itertools::Itertools;
+use maplit::hashset;
+use ndarray::*;
+use std::{collections::HashSet, *};
+
+const EMPTY: u8 = 0;
+const WALL: u8 = 1;
+const BLIZZARD_U: u8 = 2;
+const BLIZZARD_R: u8 = 4;
+const BLIZZARD_D: u8 = 8;
+const BLIZZARD_L: u8 = 16;
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Pos {
+    r: i32,
+    c: i32,
+}
+
+impl Pos {
+    fn neighbors(&self) -> Vec<Pos> {
+        vec![
+            self.clone(),
+            Pos {
+                r: self.r - 1,
+                c: self.c,
+            },
+            Pos {
+                r: self.r + 1,
+                c: self.c,
+            },
+            Pos {
+                r: self.r,
+                c: self.c - 1,
+            },
+            Pos {
+                r: self.r,
+                c: self.c + 1,
+            },
+        ]
+    }
+}
+
+struct State {
+    grid: Array2<u8>,
+    me: HashSet<Pos>,
+}
+
+fn bit_set(val: u8, bit: u8) -> bool {
+    val & bit != 0
+}
+
+impl State {
+    fn print(&self) {
+        for r in 0..self.grid.len_of(Axis(0)) {
+            for c in 0..self.grid.len_of(Axis(1)) {
+                let as_char = if self.me.contains(&Pos {
+                    r: r as i32,
+                    c: c as i32,
+                }) {
+                    'E'
+                } else {
+                    match self.grid[[r, c]] {
+                        EMPTY => '.',
+                        WALL => '#',
+                        BLIZZARD_U => '^',
+                        BLIZZARD_R => '>',
+                        BLIZZARD_D => 'v',
+                        BLIZZARD_L => '<',
+                        x => x.count_ones().to_string().chars().next().unwrap(),
+                    }
+                };
+
+                print!("{as_char}");
+            }
+
+            println!();
+        }
+    }
+
+    fn step(&self) -> Self {
+        let mut next = Self {
+            grid: Array2::zeros(self.grid.raw_dim()),
+            me: HashSet::new(),
+        };
+
+        let n_rows = self.grid.len_of(Axis(0));
+        let n_cols = self.grid.len_of(Axis(1));
+
+        for r in 0..n_rows {
+            for c in 0..n_cols {
+                let curr = self.grid[[r, c]];
+
+                if curr == WALL {
+                    next.grid[[r, c]] = WALL;
+                } else {
+                    if bit_set(curr, BLIZZARD_U) {
+                        if r == 1 {
+                            next.grid[[n_rows - 2, c]] |= BLIZZARD_U;
+                        } else {
+                            next.grid[[r - 1, c]] |= BLIZZARD_U;
+                        }
+                    }
+
+                    if bit_set(curr, BLIZZARD_D) {
+                        if r == n_rows - 2 {
+                            next.grid[[1, c]] |= BLIZZARD_D;
+                        } else {
+                            next.grid[[r + 1, c]] |= BLIZZARD_D;
+                        }
+                    }
+
+                    if bit_set(curr, BLIZZARD_L) {
+                        if c == 1 {
+                            next.grid[[r, n_cols - 2]] |= BLIZZARD_L;
+                        } else {
+                            next.grid[[r, c - 1]] |= BLIZZARD_L;
+                        }
+                    }
+
+                    if bit_set(curr, BLIZZARD_R) {
+                        if c == n_cols - 2 {
+                            next.grid[[r, 1]] |= BLIZZARD_R;
+                        } else {
+                            next.grid[[r, c + 1]] |= BLIZZARD_R;
+                        }
+                    }
+                }
+            }
+        }
+
+        for pos in self.me.iter() {
+            for adj in pos.neighbors() {
+                if 0 <= adj.r
+                    && adj.r < (n_rows as i32)
+                    && 0 <= adj.c
+                    && adj.c < (n_cols as i32)
+                    && next.grid[[adj.r as usize, adj.c as usize]] == EMPTY
+                {
+                    next.me.insert(adj);
+                }
+            }
+        }
+
+        next
+    }
+}
+
+fn parse_input(input: &str) -> State {
+    let v = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| match c {
+                    '.' => EMPTY,
+                    '#' => WALL,
+                    '^' => BLIZZARD_U,
+                    '>' => BLIZZARD_R,
+                    'v' => BLIZZARD_D,
+                    '<' => BLIZZARD_L,
+                    _ => panic!(),
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    State {
+        grid: Array2::from_shape_vec((v.len(), v[0].len()), v.concat()).unwrap(),
+        me: hashset! {Pos { r: 0, c: 1 }},
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut state = parse_input(input);
+    let end = Pos {
+        r: state.grid.len_of(Axis(0)) as i32 - 1,
+        c: state.grid.len_of(Axis(1)) as i32 - 2,
+    };
+
+    let mut minutes = 0;
+    loop {
+        if state.me.contains(&end) {
+            return Some(minutes);
+        }
+
+        minutes += 1;
+        state = state.step();
+    }
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut state = parse_input(input);
+
+    let start = Pos { r: 0, c: 1 };
+    let end = Pos {
+        r: state.grid.len_of(Axis(0)) as i32 - 1,
+        c: state.grid.len_of(Axis(1)) as i32 - 2,
+    };
+
+    let mut minutes = 0;
+
+    state.me = hashset! {start.clone()};
+    loop {
+        if state.me.contains(&end) {
+            break;
+        }
+
+        minutes += 1;
+        state = state.step();
+    }
+
+    state.me = hashset! {end.clone()};
+    loop {
+        if state.me.contains(&start) {
+            break;
+        }
+
+        minutes += 1;
+        state = state.step();
+    }
+
+    state.me = hashset! {start.clone()};
+    loop {
+        if state.me.contains(&end) {
+            break;
+        }
+
+        minutes += 1;
+        state = state.step();
+    }
+
+    Some(minutes)
 }
 
 fn main() {
@@ -19,12 +244,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 24);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(18));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 24);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(54));
     }
 }
